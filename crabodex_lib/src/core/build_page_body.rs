169e667dce677b85c70619391d0f1affcb206f3f
fabien huet <fabien.huet@gmail.com>
2024-08-25 14:31:26 +0200

    add the optional addition to the markdown generation

--- crabodex_lib/src/core/build_page_body.rs
+++ crabodex_lib/src/core/build_page_body.rs
@@ -1,20 +1,52 @@
-use pulldown_cmark::{Parser, html::push_html};
+use pulldown_cmark::{html::push_html, Options, Parser};
+
 use crate::DocNode;
 
-pub fn build_page_body(root: &DocNode, depth: usize) -> String {
+pub fn build_page_body(
+    root: &DocNode,
+    depth: usize,
+    repo_url: &str,
+) -> String {
     let mut html: String = String::new();
+    let mut options: Options = Options::empty();
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_DEFINITION_LIST);
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_GFM);
+    options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+    options.insert(Options::ENABLE_MATH);
+    options.insert(Options::ENABLE_PLUSES_DELIMITED_METADATA_BLOCKS);
+    options.insert(Options::ENABLE_SMART_PUNCTUATION);
+    options.insert(Options::ENABLE_STRIKETHROUGH);
 
     if depth > 0 {
-        let header_level = std::cmp::min(depth, 6);
-        let id = root.path.replace(' ', "-").to_lowercase();
-        html.push_str(&format!("<h{0} id=\"{1}\">{2}</h{0}>", header_level, id, root.title));
+        let header_level: usize = std::cmp::min(depth, 6);
+        let id: String = root.path.replace(' ', "-").to_lowercase();
 
-        // Compile markdown content
+        if root.file_path.is_some() {
+            let github_link: String = format!("{}/blob/main/{}", repo_url, root.clone().file_path.unwrap());
+            html.push_str(&format!(
+                "<h{0} id=\"{1}\"><span>{2} <a href=\"{3}\" title=\"View on Github\" class=\"gh-icon\"></a></span></h{0}>",
+                header_level,
+                id,
+                root.title,
+                github_link
+            ));
+        } else {
+            html.push_str(&format!(
+                "<h{0} id=\"{1}\"><span>{2}</span></h{0}>",
+                header_level,
+                id,
+                root.title
+            ));
+        }
+
         if let Some(content) = &root.content {
-            let mut content_html = String::new();
-            let parser = Parser::new(content);
+            let mut content_html: String = String::new();
+            let parser: Parser = Parser::new_ext(content, options);
             push_html(&mut content_html, parser);
-            html.push_str(&content_html);
+            html.push_str(&format!("<div class=\"depth-{}\">{}</div>", depth, content_html));
         }
     }
 
@@ -29,7 +61,7 @@ pub fn build_page_body(root: &DocNode, depth: usize) -> String {
     });
 
     for (_, child) in children {
-        html.push_str(&build_page_body(child, depth + 1));
+        html.push_str(&build_page_body(child, depth + 1, repo_url));
     }
 
     html
@@ -38,8 +70,10 @@ pub fn build_page_body(root: &DocNode, depth: usize) -> String {
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::find_markdown_files;
+
     use crate::core::build_doc_structure::build_doc_structure;
+    use crate::find_markdown_files;
+
     use super::*;
 
     #[test]
@@ -49,22 +83,10 @@ mod tests {
             .join("test_files");
 
         let markdown_files: Vec<PathBuf> = find_markdown_files(test_dir.clone());
-        let doc_structure: DocNode = build_doc_structure(&markdown_files);
-        let page_body: String = build_page_body(&doc_structure, 0);
+        let doc_structure: DocNode = build_doc_structure(&markdown_files, test_dir.as_path());
+        let page_body: String = build_page_body(&doc_structure, 0, "https://github.com/example/repo");
 
-        assert!(page_body.contains("<h1 id=\"getting-started\">Getting Started</h1>"));
-        assert!(page_body.contains("<h1 id=\"features\">Features</h1>"));
-        assert!(page_body.contains("<h1 id=\"domain\">Domain</h1>"));
-
-        assert!(page_body.contains("<h2 id=\"getting-started->-configuration\">Configuration</h2>"));
-        assert!(page_body.contains("<h2 id=\"features->-feature-one\">Feature one</h2>"));
-        assert!(page_body.contains("<h2 id=\"domain->-subdomain-one\">Subdomain one</h2>"));
-        assert!(page_body.contains("<h2 id=\"domain->-subdomain-two\">Subdomain two</h2>"));
-
-        let getting_started_index = page_body.find("Getting Started").unwrap();
-        let features_index = page_body.find("Features").unwrap();
-        let domain_index = page_body.find("Domain").unwrap();
-        assert!(getting_started_index < features_index);
-        assert!(features_index < domain_index);
+        assert!(page_body.contains("<h1 id=\"getting-started\"><span>Getting Started <a href=\"https://github.com/example/repo/blob/main/file1.md\" title=\"View on Github\" class=\"gh-icon\"></a></span></h1>"));
+        assert!(page_body.contains("<h2 id=\"getting-started->-configuration\"><span>Configuration <a href=\"https://github.com/example/repo/blob/main/sub_dir_2/file2.md\" title=\"View on Github\" class=\"gh-icon\"></a></span></h2>"));
     }
 }
