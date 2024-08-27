96fde1327d4dfabc02249cb68f5d9da1d172e93e
fabien huet <fabien.huet@gmail.com>
2024-08-28 00:43:24 +0200

    init the code for the end binary

--- crabodex_cli/src/main.rs
+++ crabodex_cli/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use std::path::PathBuf;
+use clap::Parser;
+use crabodex_lib;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(long, default_value = ".")]
+    root_directory: PathBuf,
+
+    #[clap(long, default_value = "Documentation")]
+    repo_name: String,
+
+    #[clap(long, default_value = "")]
+    repo_description: String,
+
+    #[clap(long, default_value = "latest")]
+    commit_hash: String,
+
+    #[clap(long, default_value = "latest")]
+    repo_url: String,
+
+    #[clap(long, use_value_delimiter = true, value_delimiter = ',')]
+    ignore_folders: Vec<String>,
+}
+
+fn main() -> io::Result<()> {
+    let cli: Cli = Cli::parse();
+
+    let ignore_folders: Vec<&str> = cli.ignore_folders.iter().map(AsRef::as_ref).collect();
+
+    let result: String = crabodex_lib::generate(
+        &cli.root_directory,
+        &cli.repo_name,
+        &cli.repo_description,
+        &cli.commit_hash,
+        &cli.repo_url,
+        if ignore_folders.is_empty() { None } else { Some(&ignore_folders) }
+    );
+
+    io::stdout().write_all(result.as_bytes())?;
+    Ok(())
 }
